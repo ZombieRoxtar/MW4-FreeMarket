d3094586cc6f0ca67a4f49f62650c165c67893d3
Chris Roxby <DuctTape364@Yahoo.com>
2025-01-06 15:40:07 -0500

    Corrected Comments
    Minor code cleaning

--- WriteFM.py
+++ WriteFM.py
@@ -13,10 +13,9 @@ if len(sys.argv) <= 1:
 name = sys.argv[1]
 
 with open(name, 'wb') as w:
-    # The fist eight bytes are the length of the header including itself
-    w.write(b'\x1C\x00\x00\x00')
-    w.write(b'\x00\x00\x00\x00')
-    w.write(b'\xFF\xFF\xFF\x7F') # This is also the week seperator
+    w.write(b'\x1C\x00\x00\x00') # The fist 4 bytes are the length of the
+    w.write(b'\x00\x00\x00\x00') # week separator, which immediately follows
+    w.write(b'\xFF\xFF\xFF\x7F')
     w.write(b'\xFF\xFF\xFF\x7F')
     w.write(b'\xFF\xFF\xFF\x7F')
     w.write(b'\xFF\xFF\xFF\x7F')
@@ -24,8 +23,8 @@ with open(name, 'wb') as w:
     w.write(b'\x00\x00\x00\x00') # Starting Week number
     w.write(b'\x01\x00\x00\x00') # Weapon Count
     w.write(b'\x05\x00\x00\x00') # Name Length
-    w.write("ssrm2\0".encode())  # Name, Null Terminated
+    w.write(b"ssrm2\0")          # Name, Null Terminated
     w.write(b'\x01\x00\x00\x00') # Mech Count
     w.write(b'\x05\x00\x00\x00') # Name Length
-    w.write("atlas\0".encode())  # Name, Null Terminated
+    w.write(b"atlas\0")          # Name, Null Terminated
     w.write(b'\x00\x00\x00\x00') # Footer for the end of the last week
